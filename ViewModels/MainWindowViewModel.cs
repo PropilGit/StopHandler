f7e13d367097787cf73c3e3beeac5271006e2cca
Propil <vova.khlp@yandex.ru>
2021-06-09 01:15:01 +0800

    Отказался от Controller-а Нужно привязать событие закрытия окна к функции VM

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -11,13 +11,55 @@ namespace StopHandler.ViewModels
 {
     class MainWindowViewModel : ViewModel
     {
-        Controller controller;
-        public string Log { get => controller.Log; }
+        POSTServer _MyPOSTServer;
 
         public MainWindowViewModel()
         {
-            controller = new Controller();
+            _MyPOSTServer = POSTServer.GetInstance();
+            _MyPOSTServer.onLogUpdate += AddLog;
+            _MyPOSTServer.onPOSTRequest += OnPOSTRequest;
+
+            _MyPOSTServer.Start();
         }
+        ~MainWindowViewModel()
+        {
+            _MyPOSTServer.Stop();
+        }
+
+        #region Log
+
+        private string _Log = "";
+        public string Log { get => _Log; set => Set(ref _Log, value); }
+        public void AddLog(string msg)
+        {
+            Log += "[" + DateTime.Now + "] " + msg + "\r\n";
+        }
+
+        #endregion
+
+        #region POST Request
+
+        void OnPOSTRequest(IPOSTCommand cmd)
+        {
+
+        }
+
+        #endregion
+
+        #region CloseApplicationCommand
+
+        public ICommand CloseApplicationCommand { get; }
+
+        private bool CanCloseApplicationCommandExecute(object p) => true;
+
+        private void OnCloseApplicationCommandExecuted(object p)
+        {
+            _MyPOSTServer.Stop();
+            (RootObject as Window)?.Close();
+            //Application.Current.Shutdown();
+        }
+
+        #endregion
     }
 }
 
